51295cea179c39cde1859f8943e7fafcb1cebd3e
hammam <hammam.a@munjz.com>
2020-11-27 13:19:06 +0300

    Initial Commit

---
+++ WainNakel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WainNakel
+//
+//  Created by hammam abdulaziz on 12/04/1442 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
